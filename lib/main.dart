23e85b9d0bf57e63f33f7160620b4f2987a32e86
a kandasamy <akandasamy@as-MacBook-Pro.local>
2024-01-13 12:27:00 -0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Capsule',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Porn'),
+      home: const MyHomePage(title: 'CAPSULE'),
     );
   }
 }
